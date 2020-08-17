a89adb16956a22c91ec2ca98082cca2a11336d5d
Gustavo Felix <37117188+GusFX@users.noreply.github.com>
2020-08-17 19:42:34 -0300

    Removing unused parameters

--- grammar_parser.py
+++ grammar_parser.py
@@ -1,30 +1,6 @@
 from parglare import Parser, Grammar
 
-grammar = r"""
-E: E '+' E  {left, 1}
- | E '-' E  {left, 1}
- | E '*' E  {left, 2}
- | E '/' E  {left, 2}
- | E '^' E  {right, 3}
- | '(' E ')'
- | number;
-
-terminals
-number: /\d+(\.\d+)?/;
-"""
-
 file_name = "entrada.pg"
-
-actions = {
-    "E": [lambda _, n: n[0] + n[2],
-          lambda _, n: n[0] - n[2],
-          lambda _, n: n[0] * n[2],
-          lambda _, n: n[0] / n[2],
-          lambda _, n: n[0] ** n[2],
-          lambda _, n: n[1],
-          lambda _, n: n[0]],
-    "number": lambda _, value: float(value),
-}
 
 g = Grammar.from_file(file_name)
 parser = Parser(g, debug=True)
